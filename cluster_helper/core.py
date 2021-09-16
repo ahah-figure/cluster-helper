8e32919758a77bf8b02b157b5ff4ca82c7443c6c
ahah <ahah@figure.com>
2021-09-16 18:32:13 +0000

    don't install deps

--- cluster_helper/core.py
+++ cluster_helper/core.py
@@ -5,19 +5,22 @@ from plnet.util import init_ray
 
 
 @ray.remote(num_cpus=os.cpu_count())
-def install_wheel_local(wheel_path: str) -> None:
+def install_wheel_local(wheel_path: str, install_deps: bool = False) -> None:
     os.system(f"/snap/bin/gsutil cp {wheel_path} /tmp")
     wheel_filename = os.path.basename(wheel_path)
-    os.system(f"sudo /opt/conda/default/bin/pip install /tmp/{wheel_filename}")
+    pip_cmd = f"sudo /opt/conda/default/bin/pip install /tmp/{wheel_filename} --force-reinstall"
+    if not install_deps:
+        pip_cmd = f"{pip_cmd} --no-deps"
+    os.system(pip_cmd)
 
 
-def install_wheel_cluster(wheel_path) -> None:
+def install_wheel_cluster(wheel_path, install_deps: bool) -> None:
     init_ray(distributed=True)
     ray.get([
-        install_wheel_local.remote(wheel_path=wheel_path)
+        install_wheel_local.remote(wheel_path=wheel_path, install_deps=install_deps)
         for _ in range(len(ray.nodes()))
     ])
 
 
 if __name__ == "__main__":
-    install_wheel_cluster("gs://plnet/data/wheel/plnet-0.1-py3-none-any.whl")
+    install_wheel_cluster("gs://plnet/data/wheel/plnet-0.1-py3-none-any.whl", False)
